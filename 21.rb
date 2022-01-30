c634eff0f41864060ee8dfd3ffd34961a57c6e70
Chris Douglass <cedouglass@gmail.com>
2022-01-30 15:46:38 -0500

    21 is mostly functional although rubocop hates it

--- 21.rb
+++ 21.rb
@@ -1,30 +1,14 @@
-# Logical Steps
-
-# initialize deck
-
-
-# deal one card from deck to player, removing card
-# deal one card from deck to dealer, removing card
-# deal one card from deck to player, removing card
-# deal one card from deck to dealer, removing card
-# reveal player hand to player, summing total *1
-# reveal only one card from dealer hand unless dealer value == 21
-# unless player hand == 21, ask player if they'd like to hit until total >= 21 or player says no
-#   if player hand > 21, player loss
-# unless dealer hand == 21, dealer hits until hand >= 17
-#   if dealer hand > 21, dealer loss
-# if player hand > dealer hand, else dealer wins unless equal
-#   play again?
-require 'pry'
 def prompt(text)
-  puts "=> #{text}"
+  puts "=> " + text.to_s
 end
 
 def initialize_deck
-  %w(ace one two three four five six seven eight nine ten jack queen king).map { |card| ((card.capitalize + ' ') * 4).split }
+  deck =  %w(ace one two three four five six seven eight nine ten) +
+          %w(jack queen king)
+  deck.map { |card| ((card.capitalize + ' ') * 4).split }
 end
 
-def value(card, total = 0)
+def value(card)
   case card
   when 'One' then 1
   when 'Two' then 2
@@ -39,8 +23,6 @@ def value(card, total = 0)
   when 'Jack' then 10
   when 'Queen' then 10
   when 'King' then 10
-  when 'Ace'
-    total >= 21 ? 1 : 11
   end
 end
 
@@ -50,8 +32,17 @@ end
 
 def total(hand)
   if hand.include?('Ace')
-    non_ace = hand.select { |card| card != 'Ace' && card != nil }.map { |card| value(card) }.sum
-    ace = hand.select { |card| card == 'Ace' && card != nil }.map { |card| non_ace > 10 ? card = 1 : card = 11 }.sum
+    non_ace = hand.select { |card| card != 'Ace' && !card.nil? }
+    non_ace = non_ace.map { |card| value(card) }.sum
+    if hand.select { |card| card == 'Ace' && !card.nil? }.map.size < 2
+      ace = hand.select { |card| card == 'Ace' && !card.nil? }
+      ace = ace.map { |card| non_ace > 10 ? card = 1 : card = 11 }.sum
+    else
+      (hand.select { |card| card == 'Ace' && !card.nil? }.map.size).times do
+        non_ace + 11 > 21 ? non_ace += 1 : non_ace += 11
+      end
+      ace = 0
+    end
     non_ace + ace
   else
     hand.map { |cards| value(cards) }.sum
@@ -59,51 +50,166 @@ def total(hand)
 end
 
 def bust?(hand)
-  total(hand) > 21 ? true : false
+  total(hand) > 21
 end
 
 def hit(hand, deck)
   hand << deal_card!(deck)
-  system 'clear'
 end
 
-def player_turn(player_hand, dealer_hand, deck)
+def player_turn(player_hand, deck)
   loop do
-    prompt('Your hand is:')
-    puts player_hand
-    prompt('Your total is:')
-    prompt(total(player_hand))
-    prompt('The dealer is showing:')
-    puts dealer_hand.sort[0]
+    if bust?(player_hand)
+      prompt('You have busted!')
+      return
+    end
+
     prompt('Would you like to: (1) Hit or (2) Stand?')
     answer = gets.chomp
-    if answer == '1'
+    system 'clear'
+    if answer.start_with?('1') || answer.start_with?('2')
+
+      if answer == '1'
         hit(player_hand, deck)
-    elsif answer == '2' || bust?(player_hand)
-      break
+        prompt('Your hand is:')
+        puts player_hand
+        prompt('Your total is:')
+        puts total(player_hand)
+      else
+        'Stands'
+        break
+      end
+
     else
-      prompt 'Please select a valid response.'
+      prompt('Please select a valid response')
+      next
     end
   end
 end
 
-deck = initialize_deck
+def dealer_turn(dealer_hand, player_hand, deck)
+  loop do
+    dealer_total = total(dealer_hand)
 
-player_total = [0]
-dealer_total = [0]
+    if bust?(dealer_hand)
+      prompt('Dealer hand is:')
+      puts dealer_hand
+      prompt('Dealer total is:')
+      puts total(dealer_hand)
+      prompt('Dealer has busted!')
+      return
+    end
 
-player_hand = [deal_card!(deck)]
-dealer_hand = [deal_card!(deck)]
-player_hand << deal_card!(deck)
-dealer_hand << deal_card!(deck)
+    if dealer_total < 17
+      dealer_total > total(player_hand) ? break : hit(dealer_hand, deck)
+    else
+      break
+    end
+  end
+end
 
-player_total = total(player_hand)
-dealer_total = total(dealer_hand)
+def determine_winner(hand1, hand2)
+  if bust?(hand1) == false && total(hand1) > total(hand2)
+    'win'
+  elsif bust?(hand2) == false && total(hand2) > total(hand1)
+    'lose'
+  elsif (total(hand1) == total(hand2)) || (bust?(hand1) && bust(hand2))
+    'tie'
+  end
+end
 
-prompt('Welcome to 21!')
-
-answer = ''
+player_chips = 2000
+bet = 0
 
 loop do
-  player_turn(player_hand, dealer_hand, deck)
+  system 'clear'
+  prompt('Welcome to 21!')
+
+  loop do
+    prompt('Chips:')
+    puts player_chips
+    prompt('How much would you like to bet:')
+    prompt('25, 50, 100')
+    bet = gets.chomp.to_i
+    break if [25, 50, 100].include?(bet)
+    prompt('Please enter a valid response')
+  end
+
+  system 'clear'
+  deck = initialize_deck
+  bust = false
+  player_hand = Array.new
+  dealer_hand = Array.new
+  player_total = 0
+  dealer_total = 0
+  player_hand = [deal_card!(deck)]
+  dealer_hand = [deal_card!(deck)]
+  player_hand << deal_card!(deck)
+  dealer_hand << deal_card!(deck)
+
+  prompt('Your hand is:')
+  puts player_hand
+  prompt('Your total is:')
+  prompt(total(player_hand))
+
+  prompt('The dealer is showing:')
+  puts dealer_hand.min
+
+  next if player_turn(player_hand, deck) == 'Stands'
+  if bust?(player_hand)
+    bust = true
+    player_chips -= bet
+    player_hand = Array.new
+    dealer_hand = Array.new
+    prompt('Thanks for playing!')
+    prompt('Play again? (Y/N)')
+    play_again = gets.chomp
+    break if play_again.downcase.start_with?('n')
+  end
+
+  dealer_turn(dealer_hand, player_hand, deck)
+  if bust?(dealer_hand)
+    bust = true
+    player_chips += bet
+    player_hand = Array.new
+    dealer_hand = Array.new
+    prompt('Thanks for playing!')
+    prompt('Play again? (Y/N)')
+    play_again = gets.chomp
+    break if play_again.downcase.start_with?('n')
+  end
+
+  system 'clear'
+  dealer_total = total(dealer_hand)
+  player_total = total(player_hand)
+  prompt('Player hand is:')
+  puts player_hand
+  prompt('Player total is ' + player_total.to_s)
+  prompt('Dealer hand is:')
+  puts dealer_hand
+  prompt('Dealer total is ' + dealer_total.to_s)
+
+  if bust == false
+    winner = determine_winner(player_hand, dealer_hand)
+    if winner == 'win'
+      player_chips += bet
+      prompt('Player wins!')
+    elsif winner == 'lose'
+      player_chips -= bet
+      prompt('Dealer wins!')
+    elsif winner = 'tie'
+      prompt("It's a tie!")
+    end
+  else
+    next
+  end
+
+  prompt('Play again? (Y/N)')
+  play_again = gets.chomp
+  if play_again.downcase.start_with?('n')
+    prompt('Thanks for playing 21!')
+    prompt('Goodbye!')
+    break
+  else next
+  end
 end
